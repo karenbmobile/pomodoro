798811d27d3c1cd7b70c5e197bd8f550388c87c6
Karen Brown <karenb.mobile@me.com>
2017-10-28 06:04:40 +1300

    removed core data

--- TaskMaster/AppDelegate.swift
+++ TaskMaster/AppDelegate.swift
@@ -53,29 +53,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        saveContext()
     }
 
-    //MARK: - Core Data Stack
-    lazy var persistentContainer:NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "TaskModel")
-        container.loadPersistentStores(completionHandler: {(storeDescription:NSPersistentStoreDescription, error: Error?) in
-            if let error = error as NSError? {
-                print(error.localizedDescription)
-            }
-        })
-        return container
-    }()
-    
-    func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
-    }
+
 }
 
--- TaskMaster/TasksTableViewController.swift
+++ TaskMaster/TasksTableViewController.swift
@@ -53,27 +53,12 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
     //save new task
     func save(description: String) {
         
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let entity = NSEntityDescription.entity(forEntityName: "Tasks", in: managedContext)!
-        
-        let task = NSManagedObject(entity: entity, insertInto: managedContext)
-        
+        // save task
+        /*
         task.setValue(description, forKeyPath: "taskDescription")
         task.setValue(NSDate(), forKey: "dateAdded")
         task.setValue(false, forKey: "doneIndicator")
-        
-        do {
-            try managedContext.save()
-        } catch let error as NSError {
-            
-            print("Could not save. \(error), \(error.userInfo)")
-            
-        }
+ */
         
     }
         
@@ -82,8 +67,6 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -92,86 +75,11 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
         //self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
-    // MARK: - Fetched results controller
     
-    var fetchedResultsController: NSFetchedResultsController<Tasks> {
-        if _fetchedResultsController != nil {
-            return _fetchedResultsController!
-        }
-        
-        let fetchRequest: NSFetchRequest<Tasks> = Tasks.fetchRequest()
-        
-        // Set the batch size to a suitable number.
-        fetchRequest.fetchBatchSize = 20
-        
-        // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "dateAdded", ascending: true)
-        
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        // Edit the section name key path and cache name if appropriate.
-        // nil for section name key path means "no sections".
-        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: "doneIndicator", cacheName: "Master")
-        aFetchedResultsController.delegate = self
-        _fetchedResultsController = aFetchedResultsController
-        
-        do {
-            try _fetchedResultsController!.performFetch()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-        
-        return _fetchedResultsController!
-    }
-    
-    var _fetchedResultsController: NSFetchedResultsController<Tasks>? = nil
-    
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
-            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-        case .delete:
-            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-        default:
-            return
-        }
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            tableView.insertRows(at: [newIndexPath!], with: .fade)
-        case .delete:
-            tableView.deleteRows(at: [indexPath!], with: .fade)
-        default:
-            self.configureCell(tableView.cellForRow(at: indexPath!)!, withTasks: anObject as! Tasks)
-        }
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let context = fetchedResultsController.managedObjectContext
-            context.delete(fetchedResultsController.object(at: indexPath))
-            
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+            // delete row
         }
     }
     
@@ -185,47 +93,31 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
     // MARK: - Table view
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return fetchedResultsController.sections?.count ?? 0
+        //TODO: return number of sections
+        return 1
     }
 
     //completed status to be header of section
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if let sectionInfo = fetchedResultsController.sections?[section] {
-            
-            if sectionInfo.name == "0" {
-                return "To Do"
-            }
-            else if sectionInfo.name == "1" {
-                return "Completed"
-            }
-        }
+        
         return nil
     }
     //return the number of rows grouped by completed or not completed
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionInfo = fetchedResultsController.sections![section]
-        return sectionInfo.numberOfObjects
+        //TDO: return correct number of rows
+        return 1
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let taskObject = self.fetchedResultsController.object(at: indexPath)
-        
-        //TODO: replace this with a call to configureCell once I am ready to put extra detail into the cell
-        cell.textLabel?.text = taskObject.taskDescription
+        cell.textLabel?.text = "test"
 
         return cell
     }
     
-    func configureCell(_ cell: UITableViewCell, withTasks borrowItem: Tasks) {
-        
-        //configure the table row cells
-        //add a checkbox button (true or false)
-        //add the taskDescription
-        
-    }
+    
 
     /*
     // Override to support conditional editing of the table view.
